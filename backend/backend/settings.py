80fe19eb016fe8c0a9fdeffc4905848ece9306e0
Ivan <vano979797@gmail.com>
2025-01-19 21:38:44 +0100

    hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,8 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-# ALLOWED_HOSTS = ['praktyaivsrvtr.ddns.net', 'backend', 'localhost', '127.0.0.1']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['51.250.30.64', 'praktyaivsrvtr.ddns.net', '127.0.0.1', 'localhost']
 
 # Application definition
 
